6eb960e7d51df909a239655a3f5fa2155e8a3741
John Dowson <jldowson@gmail.com>
2021-03-15 17:19:49 +0100

    Minor changes to getting millisecond timestamp, due to a reported compiler.

--- FSUIPC_WAPI/Logger.cpp
+++ FSUIPC_WAPI/Logger.cpp
@@ -167,11 +167,11 @@ string Logger::getCurrentTime()
 // get a precise timestamp as a string
     const auto now = std::chrono::system_clock::now();
     const auto nowAsTimeT = std::chrono::system_clock::to_time_t(now);
-    const auto nowMs = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    const auto nowMs = (std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch())).count() % 1000;
     std::stringstream nowSs;
     nowSs
         << std::put_time(std::localtime(&nowAsTimeT), "%a %b %d %Y %T")
-        << '.' << std::setfill('0') << std::setw(3) << nowMs.count();
+        << '.' << std::setfill('0') << std::setw(3) << nowMs;
     return nowSs.str();
 
 }
